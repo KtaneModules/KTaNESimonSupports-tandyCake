1c6a054e60e81b74198ec83bef2ed8898db72efb
Danny7007 <67610733+Danny7007@users.noreply.github.com>
2021-03-02 16:19:24 -0500

    eat shit
    adds "eat shit" to the module description

--- Simon Supports/Assets/SimonSupportsScript.cs
+++ Simon Supports/Assets/SimonSupportsScript.cs
@@ -12,20 +12,22 @@ public class SimonSupportsScript : MonoBehaviour {
     public KMAudio Audio;
     public KMSelectable button;
     public Material[] ledMats;
+    public GameObject[] leds;
+    public Light[] lights;
     private Color[] colorList = new Color[]
     {
-        new Color(255, 0, 0),     //red
-        new Color(21, 32, 255),   //blue
-        new Color(255, 255, 0),   //yellow
-        new Color(255, 128, 0),   //orange
-        new Color(207, 0, 207),   //magenta
-        new Color(0, 141, 0),     //green
-        new Color(255, 120, 207), //pink
-        new Color(167, 255, 0),   //lime
-        new Color(0, 255, 255),   //cyan
-        new Color(255, 255, 255)  //white
+        new Color(255, 0, 0, 255),     //red
+        new Color(21, 32, 255, 255),   //blue
+        new Color(255, 255, 0, 255),   //yellow
+        new Color(255, 128, 0, 255),   //orange
+        new Color(207, 0, 207, 255),   //magenta
+        new Color(0, 141, 0, 255),     //green
+        new Color(255, 120, 207, 255), //pink
+        new Color(167, 255, 0, 255),   //lime
+        new Color(0, 255, 255, 255),   //cyan
+        new Color(255, 255, 255, 255)  //white
     };
-    static string[] colorNames = {"Red", "Blue", "Yellow", "Orange", "Magneta", "Green", "Pink", "Lime", "Cyan", "White"}; //to get colorblind text, just use the first letter
+    static string[] colorNames = {"Red", "Blue", "Yellow", "Orange", "Magenta", "Green", "Pink", "Lime", "Cyan", "White"}; //to get colorblind text, just use the first letter
     static string[] traitNames = {"Boss", "Cruel", "Faulty", "Lookalike", "Puzzle", "Simon", "Time-Based", "Translated"};
     private bool[] edgework = {false, false, false, false, false, false, false, false};
     private int attempts = 1;
@@ -66,33 +68,69 @@ public class SimonSupportsScript : MonoBehaviour {
     {
         moduleId = moduleIdCounter++;
         button.OnInteract += delegate () { ScreenPress(); return false; };
-        //ledMats[0].SetColor("_Color", colorList[0]);
+
     }
     void Start()
     {
-        edgework[0] = (Bomb.GetOnIndicators().Count() == Bomb.GetOffIndicators().Count());    //boss
-        edgework[1] = (Bomb.GetPortPlates().Any(x => x.Length == 0));                         //cruel
-        edgework[2] = (Bomb.IsPortPresent(Port.Parallel) || Bomb.IsPortPresent(Port.Serial)); //faulty
-        edgework[3] = (Bomb.GetBatteryCount(Battery.D) == 0);                                 //lookalike
-        edgework[4] = (Bomb.GetSerialNumberNumbers().Last() % 2 == 0);                        //puzzle
-        edgework[5] = (Bomb.GetBatteryCount() > 1);                                           //simon
-        edgework[6] = (Bomb.GetBatteryCount() % 2 == 1);                                      //timebased
-        edgework[7] = (Bomb.GetSerialNumber().Any(ch => "AEIOU".Contains(ch)));               //translated
+        edgework[0] = Bomb.GetOnIndicators().Count() == Bomb.GetOffIndicators().Count();    //boss
+        edgework[1] = Bomb.GetPortPlates().Any(x => x.Length == 0);                         //cruel
+        edgework[2] = Bomb.IsPortPresent(Port.Parallel) || Bomb.IsPortPresent(Port.Serial); //faulty
+        edgework[3] = Bomb.GetBatteryCount(Battery.D) == 0;                                 //lookalike
+        edgework[4] = Bomb.GetSerialNumberNumbers().Last() % 2 == 0;                        //puzzle
+        edgework[5] = Bomb.GetBatteryCount() > 1;                                           //simon
+        edgework[6] = Bomb.GetBatteryCount() % 2 == 1;                                      //timebased
+        edgework[7] = Bomb.GetSerialNumber().Any(ch => "AEIOU".Contains(ch));               //translated
 
+        StartCoroutine(StartFlashes());
 
         Debug.LogFormat("<Simon Supports #{0}> Edgework conditions: {1} {2} {3} {4} {5} {6} {7} {8}", moduleId, edgework[0], edgework[1], edgework[2], edgework[3], edgework[4], edgework[5], edgework[6], edgework[7]);
 
         GeneratePuzzle();
+        for (int i = 0; i < 5; i++)
+        {
+            leds[i].GetComponent<MeshRenderer>().material = ledMats[col[i]];
+            lights[i].color = colorList[col[i]];
+        }
 
-        Debug.LogFormat("[Simon Supports #{0}] Chosen colors: {1}, {2}, {3}, {4}, {5}", moduleId, colorNames[col[0]], colorNames[col[1]], colorNames[col[2]], colorNames[col[3]], colorNames[col[4]]);
-        Debug.LogFormat("[Simon Supports #{0}] Chosen traits: {1}, {2}, {3}", moduleId, traitNames[tra[0]], traitNames[tra[1]], traitNames[tra[2]]);
-        Debug.LogFormat("[Simon Supports #{0}] {1}'s responses: {2}, {3}, {4}", moduleId, colorNames[col[0]], FlashLog(combo[0][0]), FlashLog(combo[1][0]), FlashLog(combo[2][0]));
-        Debug.LogFormat("[Simon Supports #{0}] {1}'s responses: {2}, {3}, {4}", moduleId, colorNames[col[1]], FlashLog(combo[0][1]), FlashLog(combo[1][1]), FlashLog(combo[2][1]));
-        Debug.LogFormat("[Simon Supports #{0}] {1}'s responses: {2}, {3}, {4}", moduleId, colorNames[col[2]], FlashLog(combo[0][2]), FlashLog(combo[1][2]), FlashLog(combo[2][2]));
-        Debug.LogFormat("[Simon Supports #{0}] {1}'s responses: {2}, {3}, {4}", moduleId, colorNames[col[3]], FlashLog(combo[0][3]), FlashLog(combo[1][3]), FlashLog(combo[2][3]));
-        Debug.LogFormat("[Simon Supports #{0}] {1}'s responses: {2}, {3}, {4}", moduleId, colorNames[col[4]], FlashLog(combo[0][4]), FlashLog(combo[1][4]), FlashLog(combo[2][4]));
-
-        Debug.LogFormat("<Simon Supports #{0}> Number of attempts: {1}", moduleId, attempts);
+        Debug.LogFormat("[Simon Supports #{0}] Hello, welcome to Steel Crate Games! Your fellow employees Simon, Simon, Simon, Simon, and Simon sit in front of you, wearing their favorite ties of {1}, {2}, {3}, {4}, {5}.", moduleId, colorNames[col[0]], colorNames[col[1]], colorNames[col[2]], colorNames[col[3]], colorNames[col[4]]);
+        Debug.LogFormat("[Simon Supports #{0}] The topics on the agenda today are {1}, {2}, and {3}, but I'm sure you already knew that.", moduleId, traitNames[tra[0]], traitNames[tra[1]], traitNames[tra[2]]);
+        if (attempts != 1)
+        {
+            Debug.LogFormat("[Simon Supports #{0}] Fun Fact!: Due to administrative issues, the scheduled date for the meeting had to be moved a total of {1} times!", moduleId, attempts - 1);
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            List<int> temp = new List<int>();
+            for (int j = 0; j < 3; j++)
+            {
+                if (combo[j][i])
+                {
+                    temp.Add(j + 1);
+                }
+            }
+            switch (temp.Count())
+            {
+                case 0: Debug.LogFormat("[Simon Supports #{0}] The Simon wearing {1} agrees with none of the topics shown.", moduleId, colorNames[col[i]]); break;
+                case 1: Debug.LogFormat("[Simon Supports #{0}] The Simon wearing {1} agrees only with topic {2}.", moduleId, colorNames[col[i]], temp[0]); break;
+                case 2: Debug.LogFormat("[Simon Supports #{0}] The Simon wearing {1} agrees with topics {2} and {3}.", moduleId, colorNames[col[i]], temp[0], temp[1]); break;
+                case 3: Debug.LogFormat("[Simon Supports #{0}] The Simon wearing {1} agrees with topics {2}, {3}, and {4}.", moduleId, colorNames[col[i]], temp[0], temp[1], temp[2]); break;
+            }
+        }
+        List<int> selfAgree = new List<int>();
+        for (int i = 0; i < 3; i++)
+        {
+            if (edgework[tra[i]])
+            {
+                selfAgree.Add(i + 1);
+            }
+        }
+        switch (selfAgree.Count())
+        {
+            case 0: Debug.LogFormat("[Simon Supports #{0}] You yourself agree with none of the topics shown.", moduleId); break;
+            case 1: Debug.LogFormat("[Simon Supports #{0}] You yourself only agree with topic {1}.", moduleId, selfAgree[0]); break;
+            case 2: Debug.LogFormat("[Simon Supports #{0}] You yourself agree with topics {1} and {2}.", moduleId, selfAgree[0], selfAgree[1]); break;
+            case 3: Debug.LogFormat("[Simon Supports #{0}] You yourself agree with topics {1}, {2}, and {3}.", moduleId, selfAgree[0], selfAgree[1], selfAgree[2]); break;
+        }
     }
 
     void GeneratePuzzle() {
@@ -131,13 +169,27 @@ public class SimonSupportsScript : MonoBehaviour {
         return (combo[a][0] == combo[b][0] && combo[a][1] == combo[b][1] && combo[a][2] == combo[b][2] && combo[a][3] == combo[b][3] && combo[a][4] == combo[b][4]);
     }
 
-    string FlashLog (bool b) {
-        return (b) ? "ON" : "OFF";
-    }
+
 
     void ScreenPress()
     {
 
+    }
+
+    void Flash(int stage)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+
+        }
+    }
+
+    IEnumerator StartFlashes()
+    {
+        while (!moduleSolved)
+        {
+            yield return null;
+        }
     }
 
     #pragma warning disable 414
